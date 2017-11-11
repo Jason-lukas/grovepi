62ecf47082f7a92fecff899036da6fa5b36a6df8
Jason-lukas <yacenty67@gmail.com>
2017-11-11 16:40:20 +0100

    Funkcja liniowa jasnosci

--- ultraled.py
+++ ultraled.py
@@ -2,13 +2,19 @@
 
 from grovepi import *
 
-led = 2
+led = 5
 sonar = 4
+max_dist = float(30)
 
 while True:
     try:
-        dist = ultrasonicRead(sonar)
-        print dist
+        dist = float(ultrasonicRead(sonar))
+        if dist <= max_dist:
+            power = int((1 - dist/max_dist)*255)
+            print power
+        else:
+            print "za daleko"
+
     except KeyboardInterrupt:
         print "koniec"
         break
