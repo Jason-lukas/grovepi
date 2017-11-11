af7779cbad284eab508252ba9582ba3223938c21
Jason-lukas <yacenty67@gmail.com>
2017-11-11 16:45:16 +0100

    ustawienie jasnosci

--- ultraled.py
+++ ultraled.py
@@ -12,9 +12,14 @@ while True:
         if dist <= max_dist:
             power = int((1 - dist/max_dist)*255)
             print power
+
         else:
             print "za daleko"
+            power = 0
+
+        analogWrite(led,power)
 
     except KeyboardInterrupt:
         print "koniec"
+        analogWrite(led,0)
         break
